d909dce27ec028f75291b12a5f22ccdb53869bd9
James Crutchley <ne0rmatrix@gmail.com>
2023-01-17 03:26:22 -0800

    minor fixes for bad comments

--- NerdNewsNavigator2/View/PlayPodcastPage.xaml.cs
+++ NerdNewsNavigator2/View/PlayPodcastPage.xaml.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -12,26 +12,5 @@ public partial class PlayPodcastPage : ContentPage
     {
         InitializeComponent();
         BindingContext = viewmodel;
-
-/* Unmerged change from project 'NerdNewsNavigator2(net7.0-maccatalyst)'
-Before:
-	}
-After:
-    }
-*/
-
-/* Unmerged change from project 'NerdNewsNavigator2(net7.0-android33.0)'
-Before:
-	}
-After:
-    }
-*/
-
-/* Unmerged change from project 'NerdNewsNavigator2(net7.0-windows10.0.19041.0)'
-Before:
-	}
-After:
-    }
-*/
     }
 }
