5b4daaa57c51e4242475b0c0d3befde543d9d4a5
James Crutchley <ne0rmatrix@gmail.com>
2023-01-25 08:59:08 -0800

    Code cleanup

--- NerdNewsNavigator2/AppShell.xaml.cs
+++ NerdNewsNavigator2/AppShell.xaml.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using NerdNewsNavigator2.View;
-
 namespace NerdNewsNavigator2;
 
 public partial class AppShell : Shell
