154a5d00774f6b761ae0486fbecb565d5fa3a86a
James Crutchley <jamescrutchley@outlook.com>
2023-03-26 06:23:25 -0700

    switched Live playback from webview to 720P media element.

--- NerdNewsNavigator2/View/LivePage.xaml.cs
+++ NerdNewsNavigator2/View/LivePage.xaml.cs
@@ -2,12 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using YoutubeExplode;
-
 using Microsoft.Maui.Controls.PlatformConfiguration;
 using Application = Microsoft.Maui.Controls.Application;
 using Platform = Microsoft.Maui.ApplicationModel.Platform;
-using YoutubeExplode.Videos;
 
 #if ANDROID
 using Views = AndroidX.Core.View;
@@ -61,6 +58,11 @@ public partial class LivePage : ContentPage
         }
         mediaElement.Handler?.DisconnectHandler();
     }
+    /// <summary>
+    /// Method Loads Video after page has finished being rendered.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void ContentPage_Loaded(object? sender, EventArgs e)
     {
         if (sender is null)
@@ -71,12 +73,49 @@ public partial class LivePage : ContentPage
         _ = LoadVideo();
         SetFullScreen();
     }
+
+    /// <summary>
+    /// Method returns 720P URL for <see cref="mediaElement"/> to Play.
+    /// </summary>
+    /// <param name="m3UString"></param>
+    /// <returns></returns>
+    public static string ParseM3UPLaylist(string m3UString)
+    {
+        var masterPlaylist = MasterPlaylist.LoadFromText(m3UString);
+        var list = masterPlaylist.Streams.ToList();
+        return list.ElementAt(list.FindIndex(x => x.Resolution.Height == 720)).Uri;
+    }
+
+    /// <summary>
+    /// Method returns the Live stream M3U Url from youtube ID.
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    public static async Task<string> GetM3U_Url(string url)
+    {
+        var content = string.Empty;
+        var client = new HttpClient();
+        var youtube = new YoutubeClient();
+        var result = await youtube.Videos.Streams.GetHttpLiveStreamUrlAsync(url);
+        var response = await client.GetAsync(result);
+        if (response.IsSuccessStatusCode)
+        {
+            content = await response.Content.ReadAsStringAsync();
+        }
+
+        return content;
+    }
+    /// <summary>
+    /// Method Starts <see cref="MediaElement"/> Playback.
+    /// </summary>
+    /// <returns></returns>
     private async Task LoadVideo()
     {
-        var youtube = new YoutubeClient();
-        mediaElement.Source = await youtube.Videos.Streams.GetHttpLiveStreamUrlAsync("F2NreNEmMy4");
+        var m3u = await LivePage.GetM3U_Url("F2NreNEmMy4");
+        mediaElement.Source = LivePage.ParseM3UPLaylist(m3u);
         mediaElement.Play();
     }
+
 #nullable disable
 
     #endregion
