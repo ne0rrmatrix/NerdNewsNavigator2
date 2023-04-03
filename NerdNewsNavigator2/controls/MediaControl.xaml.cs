1c5a5688e1a3f02c37a8a6a124586d6ba9ddb0ef
James Crutchley <jamescrutchley@outlook.com>
2023-04-03 06:43:46 -0700

    Android screen res fix

--- NerdNewsNavigator2/controls/MediaControl.xaml.cs
+++ NerdNewsNavigator2/controls/MediaControl.xaml.cs
@@ -143,14 +143,6 @@ public partial class MediaControl : ContentView
         {
             PositionSlider.Maximum = mediaElement.Duration.TotalSeconds;
         }
-        if (mediaElement.CurrentState == MediaElementState.Playing && BtnPLay.Source.ToString() != "play.png")
-        {
-            MainThread.BeginInvokeOnMainThread(() => { BtnPLay.Source = "play.png"; });
-        }
-        if (mediaElement.CurrentState == MediaElementState.Paused && BtnPLay.Source.ToString().Contains("play"))
-        {
-            MainThread.BeginInvokeOnMainThread(() => { BtnPLay.Source = "pause.png"; });
-        }
     }
     private void OnPositionChanged(object? sender, MediaPositionChangedEventArgs e)
     {
