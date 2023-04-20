6ed9f0f312783bc58880c69d233b8de2efbe6c07
James Crutchley <jamescrutchley@outlook.com>
2023-04-20 15:46:55 -0700

    rename classes, fix add empty podcast ctd

--- NerdNewsNavigator2/View/SettingsPage.xaml.cs
+++ NerdNewsNavigator2/View/SettingsPage.xaml.cs
@@ -5,20 +5,109 @@
 namespace NerdNewsNavigator2.View;
 
 /// <summary>
-/// A class that manages application settings.
+/// A class that Manages displaying Podcasts from <see cref="PodcastServices"/>
 /// </summary>
 public partial class SettingsPage : ContentPage
 {
+
     /// <summary>
-    /// Initializes a new instance of the <see cref="SettingsPage"/> instance.
+    /// Private <see cref="bool"/> which sets Full Screen Mode.
     /// </summary>
-    /// <param name="viewModel">This Pages <see cref="ViewModel"/> from <see cref="SettingsViewModel"/></param>
+    private bool FullScreenMode { get; set; } = new();
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="SettingsPage"/>
+    /// </summary>
+    /// <param name="viewModel">The <see cref="ViewModel"/> instance that is managed through this class.</param> 
     public SettingsPage(SettingsViewModel viewModel)
     {
         InitializeComponent();
         BindingContext = viewModel;
     }
 
+    /// <summary>
+    /// The Method controls Adding a <see cref="Podcast"/> to <see cref="List{T}"/> of class <see cref="Podcast"/>
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        if (sender is null || Url.Text is null)
+        {
+            return;
+        }
+        await PodcastServices.AddPodcast(Url.Text.ToString());
+        await Toast.Make("Podcast Added!.", CommunityToolkit.Maui.Core.ToastDuration.Long).Show();
+        await Shell.Current.GoToAsync($"{nameof(SettingsPage)}");
+    }
+
+    /// <summary>
+    /// The Method controls adding all default <see cref="Podcast"/> to <see cref="List{T}"/> of class <see cref="Podcast"/>
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private async void AddDefault(object sender, EventArgs e)
+    {
+        await PodcastServices.AddDefaultPodcasts();
+        await Toast.Make("Defaults Added!.", CommunityToolkit.Maui.Core.ToastDuration.Long).Show();
+    }
+
+    /// <summary>
+    /// The Method controls Removing Default <see cref="Podcast"/> from <see cref="List{T}"/> of class <see cref="Podcast"/>
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private async void RemoveDefault(object sender, EventArgs e)
+    {
+        var item = await App.PositionData.GetAllPodcasts();
+        if (item.AsEnumerable().Any(x => !x.Url.Contains("feeds.twit.tv")))
+        {
+            await PodcastServices.RemoveDefaultPodcasts();
+            await Toast.Make("Defaults removed.", CommunityToolkit.Maui.Core.ToastDuration.Long).Show();
+        }
+        else
+        {
+            await DisplayAlert("", "At least one podcast needs to be added", "Ok");
+        }
+    }
+
+    /// <summary>
+    /// The Method manages Removing a <see cref="Podcast"/> from <see cref="List{T}"/> of class <see cref="Podcast"/>
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private async void EditPodcasts(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync($"{nameof(EditPage)}");
+    }
+
+    /// <summary>
+    /// The Method resets <see cref="List{T}"/> of class <see cref="Podcast"/> to defaults.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+
+    private async void ResetPodcasts(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync($"{nameof(UpdateSettingsPage)}");
+    }
+
+    /// <summary>
+    /// The Method opens donation url in default browser
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private async void OpenUrlForDonation(object sender, EventArgs e)
+    {
+        var item = "https://www.paypal.com/donate/?business=LYEHGH249KCP2&no_recurring=0&item_name=All+donations+are+welcome.+It+helps+support+development+of+NerdNewsNavigator.+Thank+you+for+your+support.&currency_code=CAD";
+        await Browser.OpenAsync(item);
+    }
+
+    /// <summary>
+    /// Method sets screen to normal screen size.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void ContentPage_Loaded(object sender, EventArgs e)
     {
         DeviceService.RestoreScreen();
--- NerdNewsNavigator2/ViewModel/SettingsViewModel.cs
+++ NerdNewsNavigator2/ViewModel/SettingsViewModel.cs
@@ -5,39 +5,15 @@
 namespace NerdNewsNavigator2.ViewModel;
 
 /// <summary>
-/// A class that inherits form <see cref="BaseViewModel"/> and manages <see cref="SettingsPage"/>
+/// A Class that extends <see cref="BaseViewModel"/> for <see cref="SettingsPage"/>
 /// </summary>
 public partial class SettingsViewModel : BaseViewModel
 {
-    ILogger<SettingsViewModel> Logger { get; set; }
     /// <summary>
-    /// Initializes a new instance of the <see cref="SettingsViewModel"/> class.
+    /// Initializes a new instance of the <see cref="SettingsViewModel"/>
     /// </summary>
-    public SettingsViewModel(ILogger<SettingsViewModel> logger, IConnectivity connectivity) : base(logger, connectivity)
+    public SettingsViewModel(ILogger<SettingsViewModel> logger, IConnectivity connectivity)
+        : base(logger, connectivity)
     {
-        Logger = logger;
-        DeviceDisplay.MainDisplayInfoChanged += DeviceDisplay_MainDisplayInfoChanged;
-        Orientation = OnDeviceOrientationChange();
-        OnPropertyChanged(nameof(Orientation));
-        _ = GetUpdatedPodcasts();
-    }
-
-    /// <summary>
-    /// Method Deletes a <see cref="Podcast"/> from <see cref="List{T}"/> <see cref="Podcast"/> in <see cref="BaseViewModel"/>
-    /// </summary>
-    /// <param name="url"></param>
-    /// <returns></returns>
-    [RelayCommand]
-    public async Task Tap(string url)
-    {
-        await PodcastServices.Delete(url);
-        var item = Podcasts.First(x => x.Url == url);
-        if (item is null)
-        {
-            Logger.LogInformation("Podcast {Item} could not be deleted. It was not found in list of Podcasts", url);
-            return;
-        }
-        Podcasts.Remove(item);
-        Logger.LogInformation("Podcast {Item} was removed form List of Podcasts", item.Url);
     }
 }
