446e74476c6c2ed91a157bceeb41733f645fc02e
ne0rrmatrix <ne0rmatrix@gmail.com>
2024-02-15 21:24:40 -0800

    The most significant changes involve the removal of custom media control files and the shift to using the standard `MediaElement` from the Maui toolkit. The `VideoPlayerPage.xaml` file was updated to replace the custom `MediaControl` with the `MediaElement`. The `VideoPlayerPage.xaml.cs` file was also updated to implement the `IRecipient<ShowItemMessage>` interface, suggesting that it can now receive messages of type `ShowItemMessage`. Additionally, new methods for handling media playback were added to this file.
    List of changes:
    
    1. The `MediaControl.xaml` and `MediaControl.xaml.cs` files were completely removed. These files contained the code for a custom media control that included various features such as play, pause, rewind, fast forward, mute, and full screen functionality.
    
    2. The `NerdNewsNavigator2.csproj` file was updated to use version 8.0.7 of Maui instead of the previous 8.0.6-nightly.9851 version.
    
    3. The `VideoPlayerPage.xaml` file was updated to replace the custom `MediaControl` with the `MediaElement` from the Maui toolkit.
    
    4. The `VideoPlayerPage.xaml.cs` file was updated to implement the `IRecipient<ShowItemMessage>` interface. This file also includes new methods for handling media playback, such as `Seek`, `MediaStopped`, `IOSStart`, and `MediaControl_Unloaded`.

--- NerdNewsNavigator2/View/VideoPlayerPage.xaml.cs
+++ NerdNewsNavigator2/View/VideoPlayerPage.xaml.cs
@@ -12,8 +12,10 @@ namespace NerdNewsNavigator2.View;
 /// <summary>
 /// A class that Displays a Video from twit.tv.
 /// </summary>
-public partial class VideoPlayerPage : ContentPage
+public partial class VideoPlayerPage : ContentPage, IRecipient<ShowItemMessage>
 {
+    Position Pos { get; set; } = new();
+
     /// <summary>
     /// Class Constructor that initializes <see cref="VideoPlayerPage"/>
     /// </summary>
@@ -23,6 +25,90 @@ public partial class VideoPlayerPage : ContentPage
     {
         InitializeComponent();
         BindingContext = viewModel;
+        WeakReferenceMessenger.Default.Register(this);
+    }
+
+    public async void Receive(ShowItemMessage message)
+    {
+        Pos.Title = message.Value.Title;
+        Pos.Url = message.Value.Url;
+        Pos.SavedPosition = TimeSpan.Zero;
+        mediaElement.Source = message.Value.Url;
+        mediaElement.Play();
+        await Seek(message.Value);
+        WeakReferenceMessenger.Default.UnregisterAll(this);
+#if WINDOWS
+        WeakReferenceMessenger.Default.Register(this);
+#endif
+    }
+
+    /// <summary>
+    /// Manages IOS seeking for <see cref="mediaElement"/> with <see cref="Pos"/> at start of playback.
+    /// </summary>
+    /// <param name="show"></param>
+    private async Task Seek(Show show)
+    {
+        mediaElement.ShouldKeepScreenOn = true;
+
+        var positionList = await App.PositionData.GetAllPositions();
+        var result = positionList.ToList().Find(x => x.Title == show.Title);
+        if (result is not null && result.SavedPosition > TimeSpan.FromSeconds(10))
+        {
+            Pos.SavedPosition = result.SavedPosition;
+#if IOS || MACCATALYST
+            mediaElement.StateChanged += IOSStart;
+#else
+            await mediaElement.SeekTo(result.SavedPosition);
+            mediaElement.StateChanged += MediaStopped;
+#endif
+        }
+        else
+        {
+            Pos.SavedPosition = mediaElement.Position;
+            Pos.Title = show.Title;
+            await App.PositionData.UpdatePosition(Pos);
+            mediaElement.StateChanged += MediaStopped;
+        }
+    }
+    private async void MediaStopped(object sender, MediaStateChangedEventArgs e)
+    {
+        switch (e.NewState)
+        {
+            case MediaElementState.Paused:
+                if (mediaElement.Position > TimeSpan.FromSeconds(10))
+                {
+                    Pos.SavedPosition = mediaElement.Position;
+                    await App.PositionData.UpdatePosition(Pos);
+                }
+                mediaElement.StateChanged += MediaStopped;
+                break;
+        }
+    }
+    public void IOSStart(object sender, MediaStateChangedEventArgs e)
+    {
+        switch (e.NewState)
+        {
+            case MediaElementState.Playing:
+                mediaElement.StateChanged -= IOSStart;
+                mediaElement.Pause();
+                mediaElement.SeekTo(Pos.SavedPosition);
+                mediaElement.Play();
+                mediaElement.StateChanged += MediaStopped;
+                break;
+        }
+    }
+
+    private async void MediaControl_Unloaded(object sender, EventArgs e)
+    {
+        if (mediaElement.Position > TimeSpan.FromSeconds(10) && Pos.Title != string.Empty)
+        {
+            Pos.SavedPosition = mediaElement.Position;
+            await App.PositionData.UpdatePosition(Pos);
+        }
+        mediaElement.Stop();
+#if ANDROID || IOS || MACCATALYST
+        mediaElement.StateChanged -= MediaStopped;
+#endif
     }
 
 #if ANDROID || IOS16_1_OR_GREATER
